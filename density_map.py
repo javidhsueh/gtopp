f8b154e1ae7a20e52c262bda8bce983e45af54b9
tlfung <tlfung@ucdavis.edu>
2014-05-11 15:59:06 -0700

    covert data with pixel

--- density_map.py
+++ density_map.py
@@ -14,8 +14,8 @@ import sys
 GRID_WIDTH = 1800
 GRID_HEIGHT = 900
 
-TIME_FRAME_WINDOW = 30
-GAUSSIAN_RANGE = 250
+TIME_FRAME_WINDOW = 150
+#GAUSSIAN_RANGE = 250
 
 use = '''
   Require:
@@ -32,68 +32,60 @@ def main(argv):
         return
     
     filename = ""
-    output_name = ""
+    outputfolder = ""
     for arg in argv:
         ary = arg.split('=')
         if ary[0] == '-i':
             filename = ary[1]
         if ary[0] == '-o':
-            output_folder = ary[1]
+            outputfolder = ary[1]
     
     if filename == "":
         print use
         exit()
 
     # start your program
-    create_grid(filename, output_folder)
-
-
-def init():
-    for y in range(2002, 2015):
-        for m in range(1, 13):
-            s=str(y)+"-"+str(m)
-            # months[s] = []
+    create_grid(filename, outputfolder)
 
 
 def read_data(fn):
-    f=fn.split(".")[0]
-    # months = dict()
-    # for y in range(2002, 2015):
-    #     for m in range(1, 13):
-    #         s=str(y)+"-"+str(m)
-    #         months[s] = []
+    f = fn.split(".")[0]
+
     days = dict()
     raw_data = csv.reader(open(fn, 'rb'), delimiter='\t')
     for row in raw_data:
         obj = dict()
         elements = row[0].split(",")
-        date = datetime.datetime.strptime(elements[6], "%Y-%m-%dT%H:%M:%SZ")
+        mydate = datetime.datetime.strptime(elements[6], "%Y-%m-%dT%H:%M:%SZ")
         #print date.year
         #obj=[elements[0], date.day, elements[7], elements[8]]
         #obj["species"] = elements[0]
         obj["long"] = elements[7]
         obj["lat"] = elements[8]
-        onedate=str(date.year)+"-"+str(date.month)+"-"+str(date.day)
+        #onedate = str(date.year) + "-" + str(date.month) + "-" + str(date.day)
+        onedate = date(mydate.year, mydate.month, mydate.day).strftime("%Y-%m-%d")
+
+        print onedate
         if onedate in days:
             days[onedate].append(obj)
         else:
             days[onedate] = []
             days[onedate].append(obj)
-        #obj["day"] = date.day
+        # obj["day"] = date.day
 
-        time = str(date.year)+"-"+str(date.month)
+        # time = str(date.year)+"-"+str(date.month)
         # months[time].append(obj)
 
         print obj
     #print simplejson.dumps(days)
-    with open(f+"_d.json", "w") as json_file:
+    with open(f + ".json", "w") as json_file:
         json_file.write(simplejson.dumps(days))
 
 
 def read_json(fn):
-    data=[]
+    data = []
     with open(fn, "rb") as json_file:
-        raw_data=json.load(json_file)
+        raw_data = json.load(json_file)
         #raw_data=json_file.read()
         #print raw_data["2003-12"]
     #print raw_data
@@ -105,47 +97,69 @@ def genEmptyGrid():
     return grid
 
 
-def create_grid(fn,fo):
+def gaussian_table():
+    g_table = dict()
+    test = []
+    for i in range(300):
+        for j in range(300):
+            d = hypot(abs(i), abs(j))
+            g_table[d] = []
+            #print float(gaussian(1, d))
+            g_table[d].append(gaussian(1, d))
+            # if d == 499.961998556:
+            #     print d
+            #test.append(d)
+    #print max(test)
+    return g_table
+
+
+def create_grid(fn, fo):
+    g_table = gaussian_table()
+
     all_position = read_json(fn)
-    total_day=0
+    total_day = 0
     #print all_position
-    a = date(2002, 1, 1)
-    b = date(2003, 12, 31)
-    index = 0
+    dd = []
+    for single in all_position:
+        dd.append(single)
+    dd.sort()
+    start = str(dd[0]).split("-")
+    end = str(dd.pop()).split("-")
+
+    a = date(int(start[0]), int(start[1]), int(start[2]))
+    b = date(int(end[0]), int(end[1]), int(end[2]))
+    #index = 0
     table = []
     for dt in rrule(DAILY, dtstart=a, until=b):
         table.append(dt.strftime("%Y-%m-%d"))
-        index +=1
+        total_day += 1
         #print dt.strftime("%Y-%m-%d")
+    print total_day
+    #sys.exit()
 
-    for one in range(335,336):
+    for one in range(0, total_day, 30): #total_day
         grid = genEmptyGrid()
-        # seed points and decay and gaussian
+
+        # collect the points in time frame
+        candidate_points = []
+        candidate_points_table = dict()
+
+        # seed points with decay
         if one < TIME_FRAME_WINDOW:
-            for p in range(one):
-                #print table[p],"------------"
+            for p in range(one+1):
                 if table[p] in all_position:
-                    # print all_position[table[p]]
+                    #print table[p], all_position[table[p]]
                     for sample in all_position[table[p]]:
                         long = float(sample["long"])
                         lat = float(sample["lat"])
-                        (x, y) = (int(round(long))*5, (int(round(lat))+90)*5)
-
-                        # decay by time
-                        center_value = 1 * decay(one-p)
-                        grid[y][x] += center_value
-
-                        # apply gaussian to neighborhood, decay by distance
-                        for i in range(-1*GAUSSIAN_RANGE, GAUSSIAN_RANGE):
-                            for j in range(-1*GAUSSIAN_RANGE, GAUSSIAN_RANGE):
-                                if i == 0 and j == 0:
-                                    continue
-
-                                if 0 < x+i < GRID_WIDTH-1 and 0 < y+j < GRID_HEIGHT-1:
-
-                                    dist = hypot(i, j)
-                                    if dist <= GAUSSIAN_RANGE:
-                                        grid[y+j][x+i] += gaussian(center_value, dist)
+                        point = (int(round(long*5)), int(round((lat+90)*5)))
+                        (x, y) = point
+                        center_value = 10 * decay(one-p)
+                        grid[y][x] = center_value
+                        candidate_points.append(point)
+                        candidate_points_table[(x, y)] = center_value
+                        #print point
+            # print candidate_points
         else:
             for p in range(one-TIME_FRAME_WINDOW, one+1):
                 if table[p] in all_position:
@@ -153,23 +167,27 @@ def create_grid(fn,fo):
                     for sample in all_position[table[p]]:
                         long = float(sample["long"])
                         lat = float(sample["lat"])
-                        (x, y) = (int(round(long))*5, (int(round(lat))+90)*5)
-                        # decay by time
-                        center_value = 1 * decay(one-p)
-                        grid[y][x] += center_value
+                        point = (int(round(long*5)), int(round((lat+90)*5)))
+                        (x, y) = point
+                        center_value = 10 * decay(one-p)
+                        grid[y][x] = center_value
+                        candidate_points.append(point)
+                        candidate_points_table[(x, y)] = center_value
 
-                        # apply gaussian to neighborhood, decay by distance
-                        for i in range(-1*GAUSSIAN_RANGE, GAUSSIAN_RANGE):
-                            for j in range(-1*GAUSSIAN_RANGE, GAUSSIAN_RANGE):
-                                if i == 0 and j == 0:
-                                    continue
+        for j in xrange(GRID_HEIGHT):
+                for i in xrange(GRID_WIDTH):
+                    aggregated_value = 0
+                    for p_idx in range(len(candidate_points)):
+                        p = candidate_points[p_idx]
+                        dist = hypot(abs(i-p[0]), abs(j-p[1]))
+                        if dist < 300:
+                            seed_value = candidate_points_table[p]
+                            aggregated_value += seed_value * g_table[dist][0]
+                            # print "pixel(%s,%s), seed point[%s]:(%s,%s)" % (i,j,p_idx, p[0],p[1])
+                            # print "dist: %s, seed_value: %s, g=%s" % (dist, seed_value, g_table[dist][0])
 
-                                if 0 < x+i < GRID_WIDTH-1 and 0 < y+j < GRID_HEIGHT-1:
-
-                                    dist = hypot(i, j)
-                                    if dist <= GAUSSIAN_RANGE:
-                                        grid[y+j][x+i] += gaussian(center_value, dist)
-
+                    grid[j][i] = aggregated_value
+        #sys.exit()
         with open("./"+fo+"/"+str(one)+".txt", "w") as f:
             for g in grid:
                 for gg in g:
@@ -179,7 +197,7 @@ def create_grid(fn,fo):
 
 def load_grid(fn):
     new_grid = genEmptyGrid()
-    h=0
+    h = 0
     with open(fn, "r") as file:
         for d in file.read().split("/t"):
             d2 = d.split(",")
@@ -187,24 +205,21 @@ def load_grid(fn):
             d2.pop()
             # print d2
             for w in range(len(d2)):
-                new_grid[h]=d2
-            h+=1
+                new_grid[h] = d2
+            h += 1
     # print new_grid
     return new_grid
         # months = dict()
 
-# init()
-# read_data("Blue Shark.csv")
-# read_data("White Shark.csv")
-# read_data("Salmon Shark.csv")
-# read_data("Common Thresher Shark.csv")
-# read_data("Shortfin Mako Shark.csv")
-# read_data("Juvenile White Shark.csv")
-
-# create_grid("./data/Blue Shark_d.json")
-#load_grid("330_1.txt")
 
 if __name__ == '__main__':
     main(sys.argv[1:])
+    # read_data("BlueShark.csv")
+    # read_data("WhiteShark.csv")
+    # read_data("SalmonShark.csv")
+    # read_data("CommonThresherShark.csv")
+    # read_data("ShortfinMakoShark.csv") #problems
+    # read_data("JuvenileWhiteShark.csv")
 
+# create_grid("./data/Blue Shark_d.json")
 
